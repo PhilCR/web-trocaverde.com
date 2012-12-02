70d3bbb988e8295788fb8916dc844cac888f83d9
Philippe <Philippe@Sleipnir>
2012-12-02 14:34:41 -0200

    [no commit message]

---
+++ WEB/Design.php
@@ -0,0 +1,44 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TrocaVerde.com - Seu lixo reciclado como desconto para você!</title>
+        <meta name="description" content="Site de compras coletivas utilizando pontos para obtenção de descontos." />
+               
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" lang="pt-BR" >
+        <link rel="stylesheet" type="text/css" href="Styles.css"/>
+        
+        <meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0" />
+        
+    </head>
+    <body>
+        <div id="header" class="header">
+            <a href="index.php"><img src="img/logo.png" class="logobox"></a>
+            <div class="locationbox"><font style="color:rgb(00,00,255)">Localização Atual: </font><font><select id="regionlocation" name="regiao"><option value="none">Região</option></select><select id="levlocation" name="lev"><option value="none">L.E.V.</option></select></font></div>
+               
+            <div class="titlebox">
+                <p><font class="he1">TrocaVerde<font class="he2">.com</font></font></p><p><font class="he3">Seu lixo reciclado como desconto para você!</font></p>
+            </div>
+            
+            
+            <div id="login" class="login">
+                <?php include("login.php") ?>
+                
+            </div>
+        </div>
+        
+        <div id="content" class="content">
+            <?php include("ofertas.php") ?>
+        </div>
+        
+        
+        <div id="footer" class="footer">
+            copyright 2012
+        </div>
+    </body>
+    
+</html>

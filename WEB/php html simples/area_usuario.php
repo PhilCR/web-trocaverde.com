d5e8e4f61f0752ea57fd918985196de50a66daed
Jadder Bismarck S Cruz <bismarck.sc@gmail.com>
2012-12-02 02:56:54 -0200

    mais um commit de html simples

---
+++ WEB/php html simples/area_usuario.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC “-//W3C//DTD HTML 4.01 Transitional//EN”>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Troca Verde</title>
+</head>
+ 
+<body>
+	<p>Área permitida a apenas usuario clientes.</p>
+</body>
+</html>

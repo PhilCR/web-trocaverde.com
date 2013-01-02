865921de3da655d47cbad326ee1d0a6366019d5f
Jadder Bismarck S Cruz <bismarck.sc@gmail.com>
2013-01-02 12:17:31 -0200

    passando para o novo design, ainda n?o acabado

--- WEB/index.php
+++ WEB/index.php
@@ -1,63 +1,168 @@
-<!DOCTYPE HTML PUBLIC �-//W3C//DTD HTML 4.01 Transitional//EN�>
+<!DOCTYPE html>
 
-<html>
+<html lang="en">
     <head>
-        <title>TrocaVerde.com - Seu lixo reciclado como desconto para voc�!</title>
-        <meta name="description" content="Site de compras coletivas utilizando pontos para obten��o de descontos." />
-               
-        
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" lang="pt-BR" >
-        <link rel="stylesheet" type="text/css" href="Styles.css"/>
-        
-        <meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0" />
-        
+        <title>TrocaVerde.com - Seu lixo reciclado como desconto para você!</title> 
+
+        <!-- Metas descritivas -->
+        <meta name="description" content="Site de compras coletivas utilizando pontos para obtenção de descontos." />
+        <meta name="keywords" content="descontos, coletivo, compra, reciclagem, shopping" />
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" lang="pt-BR" >
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+
+        <!-- Habilitando a Responsive Design -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+        <!-- Habilitar JQuery Script -->
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="js/bootstrap.js"></script>
+
+        <!-- Habilitar Scripts Próprios -->
+        <script type="text/javascript" src="js/troca.js"></script>
+
     </head>
-    <body>
-        <div id="header" class="header">
-            <a href="index.php"><img src="img/logo.png" class="logobox"></a>
-            <div class="locationbox">
-                <table>
-                    <tr>
-                        <td>
-                            <font style="color:rgb(00,00,255)">Localiza��o Atual: </font>
-                        </td>
-                        <td width="25%">
-                            <font><select id="regionlocation" name="regiao"><option value="none">Regi�o</option></select>
-                        </td>
-                        
-                        <td width="25%">
-                            <select id="levlocation" name="lev"><option value="none">L.E.V.</option></select></font>
-                        </td>
-                    </tr>
-                    
-                    </table>
+
+<body class="container">
+	<!-- Cabeçalho [X] -->
+	<div id="header" class="container-fluid" style=" background-color:#a0cca0; border:1px solid #000000; border-radius:3px;z-index: 999;">
+
+		<div class="container-fluid">
+			<!-- Formulário de Localização de Ofertas [X] -->
+			<div class="container-fluid span6 offset2 btn-info" style="border:1px solid #004400; border-radius:1px;">
+				<div class="span2">
+					<i class="icon-map-marker icon-white"></i><b>Local</b>
+				</div>
+				<div class="span1" >
+					<select id="regionlocation" name="regiao" style="width:150px;" onChange="alterarLocal()">
+						<option value="none">Região</option>
+					</select>
+				</div>
+				<div class="span1 offset1">
+					<select id="levlocation" name="lev" style="width:100px;" onChange="alterarLocal()">
+						<option value="none">L.E.V.</option>
+					</select></font>
+				</div>
+            </div>
+
+			<!-- Menu Usuário para Mobile [X] -->
+			<div class="span1 visible-phone hidden-desktop hidden-tablet">
+				<div class="btn-group">
+				<a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-user icon-white"></i>
+					Entrar
+					<span class="caret"></span></a>
+					
+					<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+						<li>
+							 <div class="span2">
+								<form class="form-horizontal" action="autenticar.php" name="form_login" method="post">
+									<div class="input-append">
+										<input class="input-medium" type="text" name="txt_email" placeholder="Digite seu Email">
+										<span class="add-on"><i class="icon-envelope"></i></span>
+									</div>
+									<div class="input-append">
+										<input class="input-medium" type="password" name="txt_senha" placeholder="Digite sua Senha">
+										<span class="add-on"><i class="icon-lock"></i></span>
+									</div>
+									<div class="row-fluid">
+										<label class="checkbox">
+											<input type="checkbox"> Lembrar-me
+										</label> 
+										<button type="submit" class="btn btn-info btn-small">Acesso</button>
+										<button type="button" class="btn btn-small" onclick=location.href='cadastro.php'>Cadastro</button>
+									</div>
+								</form>
+							</div>                   
+						</li>
+                    </ul>  
                 </div>
-                        
-               
-            <div class="titlebox">
-                <p><font class="he1">TrocaVerde<font class="he2">.com</font></font></p>
-				<p><font class="he3">Seu lixo reciclado como desconto para voc�!</font></p>
+            </div>
+        </div>
+
+
+		<div class="container-fluid">
+		
+			<!-- Logotipo [X] -->
+			<div class="span3">
+				<a href="index.php" ><img src="img/logo.png"></a>
+			</div>                  
+
+			<!-- Área de Acesso [X] -->
+			<div id="login" class="span2 offset5 visible-desktop hidden-tablet hidden-phone">
+				 <div class="span2">
+					<form class="form-horizontal" action="autenticar.php" name="form_login" method="post" disabled>
+						<div class="input-append">
+							<input class="input-medium" type="text" name="txt_email" placeholder="Digite seu Email">
+							<span class="add-on"><i class="icon-envelope"></i></span>
+						</div>
+						<div class="input-append">
+							<input class="input-medium" type="password" name="txt_senha" placeholder="Digite sua Senha">
+							<span class="add-on"><i class="icon-lock"></i></span>
+						</div>
+						<div class="row-fluid">
+							<label class="checkbox">
+								<input type="checkbox"> Lembrar-me
+							</label> 
+							<button type="submit" class="btn btn-info btn-small">Acesso</button>
+							<button type="button" class="btn btn-small" onclick=location.href='cadastro.php'>Cadastro</button>
+						</div>
+					</form>
+				</div>                
+			</div>
+
+			<!-- Área de Acesso [X] -->
+			<div id="login" class="span3 offset3 hidden-desktop visible-tablet hidden-phone">
+				<div class="span2">
+					<form class="form-horizontal" action="autenticar.php" name="form_login" method="post" disabled>
+						<div class="input-append">
+							<input class="input-medium" type="text" name="txt_email" placeholder="Digite seu Email">
+							<span class="add-on"><i class="icon-envelope"></i></span>
+						</div>
+						<div class="input-append">
+							<input class="input-medium" type="password" name="txt_senha" placeholder="Digite sua Senha">
+							<span class="add-on"><i class="icon-lock"></i></span>
+						</div>
+						<div class="row-fluid">
+							<label class="checkbox">
+								<input type="checkbox"> Lembrar-me
+							</label> 
+							<button type="submit" class="btn btn-info btn-small">Acesso</button>
+							<button type="button" class="btn btn-small" onclick=location.href='cadastro.php'>Cadastro</button>
+						</div>
+					</form>
+				</div>
 			</div>
 			
-            <div id="login" class="login">
-                <form action="autenticar.php" name="form_login" method="post">
-					<p>E-mail:<input type="text" name="txt_email" size="30" maxlength="70"/><br>
-					Senha:<input type="password" name="txt_senha" size="30" maxlength="16"/></p>
-					<p align="right"><input class="abutton" type="submit" name="btn_entrar" value="Entrar"/>
-					<input class="abutton" type="button" name="btn_cadastro" value="Novo" onclick="location.href='cadastro.php'"></p>
-				</form>
-            </div>
-			
-        </div>
-        
-        <div id="content" class="content">
-            <?php include("ofertas.php") ?>
-        </div>
-        
-        
-        <div id="footer" class="footer">
-            copyright 2012
-        </div>
-    </body>
+		</div>
+    </div>
+
+       
+	<!-- Conteudo [X] -->
+	<div id="content" class="container alert alert-info" style="margin-bottom:0px;padding: 0px 0px 0px 0px;">
+		
+	</div>        
+
+	<!-- Rodapé [X]-->
+	<div id="footer" class="container-fluid btn-large btn-success btn-block" style="border:1px solid #000;">
+		<div  class="span3">
+			&copy; 2012 - All Rights Reserved.
+		</div>
+		<div class="span2 offset1">
+			<a href="contato.html" target="blank" title="Contate-nos" class="btn-success">Contate-nos</a>
+		</div>
+
+		<div class="span2">
+			<a href="termos.html" target="blank" title="Termos e Políticas" class="btn-success">Termos e Políticas</a>
+		</div>
+
+		<div class="span3">
+			<a href="levs.php" target="blank" title="Localização dos L.E.V.s" class="btn-success">Localização dos L.E.V.s</a>
+		</div>
+
+	</div>
+</body>
     
 </html>
--- WEB/pre_pontuar.php
+++ WEB/pre_pontuar.php
@@ -29,44 +29,107 @@ $rs = mysql_query($sql, $conn);
 $num = mysql_num_rows($rs);
 ?>
 
-<!DOCTYPE HTML PUBLIC “-//W3C//DTD HTML 4.01 Transitional//EN”>
+<!DOCTYPE html>
 
-<html>
+<html lang="en">
     <head>
-        <title>TrocaVerde.com - Seu lixo reciclado como desconto para você!</title>
+        <title>TrocaVerde.com - Seu lixo reciclado como desconto para você!</title> 
+
+        <!-- Metas descritivas -->
         <meta name="description" content="Site de compras coletivas utilizando pontos para obtenção de descontos." />
-               
-        
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" lang="pt-BR" >
-        <link rel="stylesheet" type="text/css" href="Styles.css"/>
-        
-        <meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0" />
-        
+        <meta name="keywords" content="descontos, coletivo, compra, reciclagem, shopping" />
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" lang="pt-BR" >
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+
+        <!-- Habilitando a Responsive Design -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+        <!-- Habilitar JQuery Script -->
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="js/bootstrap.js"></script>
+
+        <!-- Habilitar Scripts Próprios -->
+        <script type="text/javascript" src="js/troca.js"></script>
+
     </head>
-    <body>
-        <div id="header" class="header">
-            <a href="index.php"><img src="img/logo.png" class="logobox"></a>                        
-               
-            <div class="titlebox">
-                <p><font class="he1">TrocaVerde<font class="he2">.com</font></font></p>
-				<p><font class="he3">Seu lixo reciclado como desconto para você!</font></p>
+
+<body class="container">
+	<!-- Cabeçalho [X] -->
+	<div id="header" class="container-fluid" style=" background-color:#a0cca0; border:1px solid #000000; border-radius:3px;z-index: 999;">
+
+		<div class="container-fluid">
+
+			<!-- Menu Usuário para Mobile [X] -->
+			<div class="span1 visible-phone hidden-desktop hidden-tablet">
+				<div class="btn-group">
+					 <div class="span2">
+						<form class="form-horizontal" action="sair.php" name="form_sair" method="post">
+							<div class="row-fluid">
+								<p align="right">Olá <?php echo $_SESSION['nome']; ?>
+							</div>
+							<div class="row-fluid">
+								<button type="submit" class="btn btn-info btn-small">Sair</button>
+							</div>
+						</form>
+					</div>                   
+                </div>
+            </div>
+        </div>
+
+
+		<div class="container-fluid">
+		
+			<!-- Logotipo [X] -->
+			<div class="span3">
+				<a href="index.php" ><img src="img/logo.png"></a>
+			</div>                  
+
+			<!-- Área de Acesso [X] -->
+			<div id="login" class="span2 offset5 visible-desktop hidden-tablet hidden-phone">
+				 <div class="span2">
+					<form class="form-horizontal" action="sair.php" name="form_sair" method="post">
+						<div class="row-fluid">
+							<p align="right">Olá <?php echo $_SESSION['nome']; ?>
+						</div>
+						<div class="row-fluid">
+							<button type="submit" class="btn btn-info btn-small">Sair</button>
+						</div>
+					</form>
+				</div>                
+			</div>
+
+			<!-- Área de Acesso [X] -->
+			<div id="login" class="span3 offset3 hidden-desktop visible-tablet hidden-phone">
+				<div class="span2">
+					<form class="form-horizontal" action="sair.php" name="form_sair" method="post">
+						<div class="row-fluid">
+							<p align="right">Olá <?php echo $_SESSION['nome']; ?>
+						</div>
+						<div class="row-fluid">
+							<button type="submit" class="btn btn-info btn-small">Sair</button>
+						</div>
+					</form>
+				</div>
 			</div>
 			
-            <div id="login" class="login_pont">
-				<p align="right">Olá <?php echo $_SESSION['nome']; ?>
-				<input align="right" class="abutton" type="button" name="btn_sair" value="Sair" onclick="location.href='sair.php'"/></p>
-            </div>
-			
-        </div>
-        
-        <div align="center" id="content" class="content">
+		</div>
+    </div>
+
+       
+	<!-- Conteudo [X] -->
+	<div id="content" class="container alert alert-info" style="margin-bottom:0px;padding: 0px 0px 0px 0px;">
+
 <?php
 if($num == 0) {
 	mysql_close($conn);
 ?>
 
-			<p>Cliente não encontrado
-			<input class="abutton" type="button" name="btn_voltar" value="Voltar" onclick="location.href='pontuador_index.php'"/></p>
+		<p>Cliente não encontrado
+		<input class="abutton" type="button" name="btn_voltar" value="Voltar" onclick="location.href='pontuador_index.php'"/></p>
 	
 <?php
 } else {
@@ -78,27 +141,43 @@ if($num == 0) {
 	mysql_close($conn);
 ?>	
 		
-			<form action="pontuar.php" name="form_pontuar" method="post">
-				<input type="hidden" name="txt_email" value="<?php echo $email ?>"/>
-				<input type="hidden" name="txt_pontos" value="<?php echo $pontos ?>"/>
-				<table align="center">
-					<tr><td>Nome:</td><td><?php echo $nome ?> <?php echo $snome ?></td></tr>
-					<tr><td>Data de Nascimento:</td><td><?php echo $data ?></td></tr>
-					<tr><td></td><td></td></tr>
-					<tr><td>Pontos:</td><td><?php echo $pontos ?></td></tr>
-					<tr><td></td><td align="right"><input class="abutton" type="submit" name="btn_pontuar" value="Confirmar Pontuação"/></td></tr>
-				</table>
-			</form>
+		<form action="pontuar.php" name="form_pontuar" method="post">
+			<input type="hidden" name="txt_email" value="<?php echo $email ?>"/>
+			<input type="hidden" name="txt_pontos" value="<?php echo $pontos ?>"/>
+			<table align="center">
+				<tr><td>Nome:</td><td><?php echo $nome ?> <?php echo $snome ?></td></tr>
+				<tr><td>Data de Nascimento:</td><td><?php echo $data ?></td></tr>
+				<tr><td></td><td></td></tr>
+				<tr><td>Pontos:</td><td><?php echo $pontos ?></td></tr>
+				<tr><td></td><td align="right"><input class="abutton" type="submit" name="btn_pontuar" value="Confirmar Pontuação"/></td></tr>
+			</table>
+		</form>
 
 <?php
 }
 ?>
-        </div>
-        
-        
-        <div id="footer" class="footer">
-            copyright 2012
-        </div>
-    </body>
+	</div>        
+
+	<!-- Rodapé [X]-->
+	<div id="footer" class="container-fluid btn-large btn-success btn-block" style="border:1px solid #000;">
+		<div  class="span3">
+			&copy; 2012 - All Rights Reserved.
+		</div>
+		<div class="span2 offset1">
+			<a href="contato.html" target="blank" title="Contate-nos" class="btn-success">Contate-nos</a>
+		</div>
+
+		<div class="span2">
+			<a href="termos.html" target="blank" title="Termos e Políticas" class="btn-success">Termos e Políticas</a>
+		</div>
+
+		<div class="span3">
+			<a href="levs.php" target="blank" title="Localização dos L.E.V.s" class="btn-success">Localização dos L.E.V.s</a>
+		</div>
+
+	</div>
+</body>
     
 </html>
+
+

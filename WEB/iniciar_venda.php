6b206ef24bcc18aec03a0c0f100feca7a2d4a2c1
Jadder Bismarck S Cruz <bismarck.sc@gmail.com>
2013-01-02 12:17:31 -0200

    passando para o novo design, ainda n?o acabado

--- WEB/iniciar_venda.php
+++ WEB/iniciar_venda.php
@@ -8,64 +8,140 @@ $cod = $_POST["txt_cod"];
 $valor = $_POST["txt_valor"];
 ?>	
 
-<!DOCTYPE HTML PUBLIC “-//W3C//DTD HTML 4.01 Transitional//EN”>
+<!DOCTYPE html>
 
-<html>
+<html lang="en">
     <head>
-        <title>TrocaVerde.com - Seu lixo reciclado como desconto para você!</title>
+        <title>TrocaVerde.com - Seu lixo reciclado como desconto para você!</title> 
+
+        <!-- Metas descritivas -->
         <meta name="description" content="Site de compras coletivas utilizando pontos para obtenção de descontos." />
-               
-        
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" lang="pt-BR" >
-        <link rel="stylesheet" type="text/css" href="Styles.css"/>
-        
-        <meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0" />
-        
+        <meta name="keywords" content="descontos, coletivo, compra, reciclagem, shopping" />
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" lang="pt-BR" >
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+
+        <!-- Habilitando a Responsive Design -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+        <!-- Habilitar JQuery Script -->
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="js/bootstrap.js"></script>
+
+        <!-- Habilitar Scripts Próprios -->
+        <script type="text/javascript" src="js/troca.js"></script>
+
     </head>
-    <body>
-        <div id="header" class="header">
-            <a href="index.php"><img src="img/logo.png" class="logobox"></a>                        
-               
-            <div class="titlebox">
-                <p><font class="he1">TrocaVerde<font class="he2">.com</font></font></p>
-				<p><font class="he3">Seu lixo reciclado como desconto para você!</font></p>
+
+<body class="container">
+	<!-- Cabeçalho [X] -->
+	<div id="header" class="container-fluid" style=" background-color:#a0cca0; border:1px solid #000000; border-radius:3px;z-index: 999;">
+
+		<div class="container-fluid">
+
+			<!-- Menu Usuário para Mobile [X] -->
+			<div class="span1 visible-phone hidden-desktop hidden-tablet">
+				<div class="btn-group">
+					 <div class="span2">
+						<form class="form-horizontal" action="sair.php" name="form_sair" method="post">
+							<div class="row-fluid">
+								<p align="right">Olá <?php echo $_SESSION['razao_social']; ?>
+							</div>
+							<div class="row-fluid">
+								<button type="submit" class="btn btn-info btn-small">Sair</button>
+							</div>
+						</form>
+					</div>                   
+                </div>
+            </div>
+        </div>
+
+
+		<div class="container-fluid">
+		
+			<!-- Logotipo [X] -->
+			<div class="span3">
+				<a href="index.php" ><img src="img/logo.png"></a>
+			</div>                  
+
+			<!-- Área de Acesso [X] -->
+			<div id="login" class="span2 offset5 visible-desktop hidden-tablet hidden-phone">
+				 <div class="span2">
+					<form class="form-horizontal" action="sair.php" name="form_sair" method="post">
+						<div class="row-fluid">
+							<p align="right">Olá <?php echo $_SESSION['razao_social']; ?>
+						</div>
+						<div class="row-fluid">
+							<button type="submit" class="btn btn-info btn-small">Sair</button>
+						</div>
+					</form>
+				</div>                
+			</div>
+
+			<!-- Área de Acesso [X] -->
+			<div id="login" class="span3 offset3 hidden-desktop visible-tablet hidden-phone">
+				<div class="span2">
+					<form class="form-horizontal" action="sair.php" name="form_sair" method="post">
+						<div class="row-fluid">
+							<p align="right">Olá <?php echo $_SESSION['razao_social']; ?>
+						</div>
+						<div class="row-fluid">
+							<button type="submit" class="btn btn-info btn-small">Sair</button>
+						</div>
+					</form>
+				</div>
 			</div>
 			
-            <div id="login" class="login_loj">
-				<p align="right">Olá <?php echo $_SESSION['razao_social']; ?>
-				<input align="right" class="abutton" type="button" name="btn_sair" value="Sair" onclick="location.href='sair.php'"/></p>
-            </div>
-			
-        </div>
-        
-        <div align="center" id="content" class="content">
-			<div class="mainofferbox" align="left"><form action="confirmar_venda.php" name="form_vender" method="post">
-				<table>
-				<tr><td><img src="img/coxinha.jpeg" class=""></td>
-				<td>
-					<p>Coxinha 30% off</p>
-					<p>Cantina</p>
-					<input type="hidden" name="txt_cod" value="2"/>
-					<input type="hidden" name="txt_valor" value="200"/>
-					<p>descrição haiuhsasuidhasd sbduasbdui ad aisd asd asd asda dasj dasjk das dasm dam,s dasm,d am,sd sm
-					sdmamjsaiofb dfgddsajsna sndsjadnas iodsaiosd ns erewqwiwdqwdqwe we qwr qwe ewfw wefwe fewf ewf ewf nd
-					wdqwdwa qwe qedqw dw dq dqwq d wdqwdqwdqw d wqdqwd qqdqdqwdqwdqwdqwdqwdqwdqwdqwdwqedwed dwed wedqs</p>
-					<table width="100%">
-						<tr><td>Quantidade: 1000</td><td>Vendidos: 0</td></tr>
-						<tr><td>Valor: 200 trocados</td><td></td></tr>
-						<tr><td>Quantidade:</td><td><input type="text" name="txt_qtde" size="5" maxlength="5"/></td></tr>
-						<tr><td>CPF:</td><td><input type="text" name="txt_cpf" size="50" maxlength="11"/></td></tr>
-						<tr><td></td><td align="right"><input class="abutton" type="submit" name="btn_vender" value="Vender"/></td></tr>
-					</table>
-				</td></tr>
+		</div>
+    </div>
+
+       
+	<!-- Conteudo [X] -->
+	<div id="content" class="container alert alert-info" style="margin-bottom:0px;padding: 0px 0px 0px 0px;">
+		<form action="confirmar_venda.php" name="form_vender" method="post">
+			<table>
+			<tr><td><img src="img/coxinha.jpeg" class=""></td>
+			<td>
+				<p>Coxinha 30% off</p>
+				<p>Cantina</p>
+				<input type="hidden" name="txt_cod" value="2"/>
+				<input type="hidden" name="txt_valor" value="200"/>
+				<p>descrição haiuhsasuidhasd sbduasbdui ad aisd asd asd asda dasj dasjk das dasm dam,s dasm,d am,sd sm
+				sdmamjsaiofb dfgddsajsna sndsjadnas iodsaiosd ns erewqwiwdqwdqwe we qwr qwe ewfw wefwe fewf ewf ewf nd
+				wdqwdwa qwe qedqw dw dq dqwq d wdqwdqwdqw d wqdqwd qqdqdqwdqwdqwdqwdqwdqwdqwdqwdwqedwed dwed wedqs</p>
+				<table width="100%">
+					<tr><td>Quantidade: 1000</td><td>Vendidos: 0</td></tr>
+					<tr><td>Valor: 200 trocados</td><td></td></tr>
+					<tr><td>Quantidade:</td><td><input type="text" name="txt_qtde" size="5" maxlength="5"/></td></tr>
+					<tr><td>CPF:</td><td><input type="text" name="txt_cpf" size="50" maxlength="11"/></td></tr>
+					<tr><td></td><td align="right"><input class="abutton" type="submit" name="btn_vender" value="Vender"/></td></tr>
 				</table>
-			</form></div>
-        </div>
-        
-        
-        <div id="footer" class="footer">
-            copyright 2012
-        </div>
-    </body>
+			</td></tr>
+			</table>
+		</form>
+	</div>        
+
+	<!-- Rodapé [X]-->
+	<div id="footer" class="container-fluid btn-large btn-success btn-block" style="border:1px solid #000;">
+		<div  class="span3">
+			&copy; 2012 - All Rights Reserved.
+		</div>
+		<div class="span2 offset1">
+			<a href="contato.html" target="blank" title="Contate-nos" class="btn-success">Contate-nos</a>
+		</div>
+
+		<div class="span2">
+			<a href="termos.html" target="blank" title="Termos e Políticas" class="btn-success">Termos e Políticas</a>
+		</div>
+
+		<div class="span3">
+			<a href="levs.php" target="blank" title="Localização dos L.E.V.s" class="btn-success">Localização dos L.E.V.s</a>
+		</div>
+
+	</div>
+</body>
     
 </html>

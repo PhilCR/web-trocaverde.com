77e4fd257599c6ce9a0801fb5c0bd0986ae02d34
Jadder Bismarck S Cruz <bismarck.sc@gmail.com>
2013-01-04 18:42:17 -0200

    corre??o da mudan?a da procedure + libera??o das ofertas s? falta um requisito:
    RF06 - O sistema deve permitir o usu?rio alterar suas informa??es cadastrais.

--- BD/16_jadder_4.sql
+++ BD/16_jadder_4.sql
@@ -6,8 +6,8 @@ DELIMITER |
 
 CREATE PROCEDURE libera_oferta(app_cod INTEGER, app_pontos INTEGER)
 BEGIN
-	UPDATE oferta SET pontos = app_pontos WHERE cod = app_cod;
-    UPDATE oferta SET autorizada = 1 WHERE cod = app_cod;
+	UPDATE ofertas SET pontos = app_pontos WHERE cod = app_cod;
+    UPDATE ofertas SET autorizada = 1 WHERE cod = app_cod;
 END
 
 |

427c7c71b73b87a065ba37a796576172d409484f
Jadder Bismarck S Cruz <bismarck.sc@gmail.com>
2013-01-30 17:01:56 -0200

    corre??es avalia??o heuristica

--- WEB/ARQS_HOST_GRATIS/termos.php
+++ WEB/ARQS_HOST_GRATIS/termos.php
@@ -125,8 +125,41 @@
 
        
 	<!-- Conteudo [X] -->
-	<div id="content" align="center" class="container alert alert-info" style="margin-bottom:0px;padding: 0px 0px 0px 0px; min-height: 300px;">
-		<h3>Desculpe, caiu café na nossa documentação então ainda não publicamos os termo de use e políticas, aguarde...</h3>
+	<div id="content" align="justify" class="container alert alert-info" style="margin-bottom:0px;padding: 0px 0px 0px 0px; min-height: 300px;">
+		<h3>O <font color="00f000">Troca Verde</font> é um sistema onde seu lixo reciclado como desconto para você.</h3>
+		<br>
+		...
+		<br>
+		O lixo reciclável deve ser entregue pelo consumidor nas sacolas padronizadas do Sistema Troca Verde.
+		As sacolas padronizadas são totalmente customizadas e tem volume determinado de 50l.
+		O consumidor deverá entregar o lixo reciclável não-segregado num único grupo, 
+		o número de "Trocados" gerados será resultante de: Índice Troca Verde x Volume de "lixo reciclável" entregue
+		<br><br>
+		<h4><font color="00f000">Índice Troca Verde</font></h4><br>
+		O Índice Troca Verde é calculado da seguinte maneira:<br>
+		<b>1</b> - A composição média do lixo reciclável Fonte: CEMPRE - Ciclosoft (2010)<br>
+		<b>2</b> - O valor praticado no mercado no mês para o comércio de cada item. Fonte: Pesquisa Mensal de Mercado<br>
+		<b>3</b> - Multiplica-se a porcentagem de participação do item na composição do lixo reciclável pelo valor do peso daquele material no mercado no mês<br>
+		<b>4</b> - Somam-se todas as multiplicações<br>
+		<b>5</b> - O resultado é o Índice Troca Verde<br>
+		<br>
+		
+		<table>
+		<tr><td>Item					</td><td>% no lixo reciclável	</td><td>R$ / Kg</td><td>		</td></tr>
+		<tr><td>Papel / Papelão			</td><td>39,0%					</td><td>0,3	</td><td>R$0,117</td></tr>
+		<tr><td>Metal					</td><td>8,2%					</td><td>6		</td><td>R$0,492</td></tr>
+		<tr><td>Alumínio				</td><td>1,0%					</td><td>3		</td><td>R$0,030</td></tr>
+		<tr><td>Aço						</td><td>1,8%					</td><td>0,12	</td><td>R$0,002</td></tr>
+		<tr><td>Vidro					</td><td>14,0%					</td><td>0,12	</td><td>R$0,017</td></tr>
+		<tr><td>Plástico				</td><td>20,0%					</td><td>0,5	</td><td>R$0,100</td></tr>
+		<tr><td>PET						</td><td>14,0%					</td><td>0,95	</td><td>R$0,133</td></tr>
+		<tr><td>Embalagem Longa Vida	</td><td>2,0%					</td><td>0,49	</td><td>R$0,010</td></tr>
+		<tr><td>Índice Troca Verde		</td><td>						</td><td>		</td><td><b>R$0,901</b></td></tr>
+		</table>
+
+		<br><br>
+		O Índice Troca Verde é atualizado mensalmente.
+		
 	</div>        
 
 	<!-- Rodapé [X]-->
@@ -135,15 +168,15 @@
 			&copy; 2012 - All Rights Reserved.
 		</div>
 		<div class="span2 offset1">
-			<a href="contato.html" target="blank" title="Contate-nos" class="btn-success">Contate-nos</a>
+			<a href="contato.php" title="Contate-nos" class="btn-success">Contate-nos</a>
 		</div>
 
 		<div class="span2">
-			<a href="termos.html" target="blank" title="Termos e Políticas" class="btn-success">Termos e Políticas</a>
+			<a href="termos.php" title="Termos e Políticas" class="btn-success">Termos e Políticas</a>
 		</div>
 
 		<div class="span3">
-			<a href="levs.php" target="blank" title="Localização dos L.E.V.s" class="btn-success">Localização dos L.E.V.s</a>
+			<a href="levs.php" title="Localização dos L.E.V.s" class="btn-success">Localização dos L.E.V.s</a>
 		</div>
 
 	</div>
